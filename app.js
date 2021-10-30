d963806dd77652bd0e30fa03519964be9434e9bb
learnjsdev <learn.js.dev@gmail.com>
2021-10-30 15:21:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
+  navigator.serviceWorker.register('/sw.js', {scope: '/'})
     .then((reg) => {
       if(reg.active) {
         reg.active.postMessage('hi');
