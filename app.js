8f9dbbe8c565c09db636dc4358c06c5d22ff6dba
learnjsdev <learn.js.dev@gmail.com>
2021-10-31 13:20:12 +0200

    upd

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
-    .then((reg) => {
-      if(reg.active) {
-        reg.active.postMessage('registred');
-      }
+    .then(response => {
+      console.log(response)
     }).catch((error) => {
     console.log('Registration failed with ' + error);
-  });
-
-  navigator.serviceWorker.addEventListener('message', event => {
-    console.log(event.data.msg, event.data.url);
   });
 }
