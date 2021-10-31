0afdb500176fe8e3dd2787ceb9c3f4c8a53b48dd
learnjsdev <learn.js.dev@gmail.com>
2021-10-31 14:09:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
-  console.log(url.origin, location.origin, url);
-  debugger;
+  //console.log(url.origin, location.origin, url);
+  //debugger;
 
   event.respondWith(
     fetch(event.request).then(function(response) {
@@ -13,31 +13,32 @@ self.addEventListener('fetch', (event) => {
         // An HTTP error response code (40x, 50x) won't cause the fetch() promise to reject.
         // We need to explicitly throw an exception to trigger the catch() clause.
 
-        throw Error('response status ' + response.status);
+        //throw Error('response status ' + response.status);
+
+        console.log('error')
+
+        event.waitUntil(async function() {
+          // Exit early if we don't have access to the client.
+          // Eg, if it's cross-origin.
+          if (!event.clientId) return;
+
+          // Get the client.
+          const client = await clients.get(event.clientId);
+          // Exit early if we don't get the client.
+          // Eg, if it closed.
+          if (!client) return;
+
+          // Send a message to the client.
+          client.postMessage({
+            msg: "Hey I just got a fetch from you!",
+            url: event.request.url
+          });
+
+        }());
       }
 
       // If we got back a non-error HTTP response, return it to the page.
       return response;
-    }).catch(function(error) {
-      event.waitUntil(async function() {
-        // Exit early if we don't have access to the client.
-        // Eg, if it's cross-origin.
-        if (!event.clientId) return;
-
-        // Get the client.
-        const client = await clients.get(event.clientId);
-        // Exit early if we don't get the client.
-        // Eg, if it closed.
-        if (!client) return;
-
-        // Send a message to the client.
-        client.postMessage({
-          msg: "Hey I just got a fetch from you!",
-          url: event.request.url
-        });
-
-      }());
-      
     })
 
   );
