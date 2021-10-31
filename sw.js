c8bb90fa9de2f77195a297b438430826772370a7
learnjsdev <learn.js.dev@gmail.com>
2021-10-31 14:19:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,9 +15,28 @@ self.addEventListener('fetch', (event) => {
 
         //throw Error('response status ' + response.status);
 
-        console.dir(event)
+        //console.dir(event.resultingClientId);
 
-        debugger
+        //debugger
+
+        event.waitUntil(async function() {
+          // Exit early if we don't have access to the client.
+          // Eg, if it's cross-origin.
+          if (!event.resultingClientId) return;
+
+          // Get the client.
+          const client = await clients.get(event.resultingClientId);
+          // Exit early if we don't get the client.
+          // Eg, if it closed.
+          if (!client) return;
+
+          // Send a message to the client.
+          client.postMessage({
+            msg: "Hey I just got a fetch from you!",
+            url: event.request.url
+          });
+
+        }());
 
 
       }
