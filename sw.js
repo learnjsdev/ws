4babe08c2ad93c0f67c7411e79dc906eaf0db7b0
learnjsdev <learn.js.dev@gmail.com>
2021-10-31 13:56:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+})
+
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   console.log(url.origin, location.origin, url);
+  debugger;
 
 })
