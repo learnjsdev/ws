475debe58eb952f53a5b9116e6d541e2c53c22c0
i.melnyk <learn.js.dev@gmail.com>
2021-11-01 18:34:40 +0200

    upd

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then((response) => {
-      console.log(response)
+      return response
     }).catch((error) => {
     console.log('Registration failed with ' + error);
   });
 
   navigator.serviceWorker.addEventListener('message', (data) => {
     console.log(document.title)
-  })
+  });
 }
