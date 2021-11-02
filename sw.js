c0b017d5501dfe1e2e762033bf5e763a81bc5dba
learn.js.dev <learn.js.dev@gmail.com>
2021-11-02 19:11:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,17 @@ function checkBody(body) {
 
 self.addEventListener('fetch', event => {
   console.log('fetch', event);
+  try {
+    fetch(event.request.url).then(response => {
+      return response.clone().text();
+    }).then(body => {
+      console.log('check', checkBody(body))
+    })
+  } catch (e) {
+    console.log(e)
+  }
+
+
 
   debugger;
 
