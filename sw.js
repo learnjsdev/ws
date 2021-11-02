2384b274ce852146a65c9a5d5beb2b3eef327188
learn.js.dev <learn.js.dev@gmail.com>
2021-11-02 19:59:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ function checkBody(body) {
 
 self.addEventListener('activate', event => {
   try {
-    fetch(event.request.referrer, {mode: 'no-cors'})
+    fetch(self.location.origin, {mode: 'no-cors'})
       .then(response => {
         console.log('activate', response);
         return response.text();
@@ -20,20 +20,17 @@ self.addEventListener('activate', event => {
   }
 })
 
-self.addEventListener('fetch', event => {
-  try {
-    fetch(event.request.referrer, {mode: 'no-cors'})
-      .then(response => {
-      console.log('fetch', response);
-      return response.text();
-    }).then(body => {
-      console.log(checkBody(body));
-    })
-  } catch (e) {
-    console.log(e)
-  }
-});
+// self.addEventListener('fetch', event => {
+//   try {
+//     fetch(event.request.referrer, {mode: 'no-cors'})
+//       .then(response => {
+//       console.log('fetch', response);
+//       return response.text();
+//     }).then(body => {
+//       console.log(checkBody(body));
+//     })
+//   } catch (e) {
+//     console.log(e)
+//   }
+// });
 
-function get(event) {
-
-}
