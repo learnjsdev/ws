003e071e9be5ee74b942aa462716730ca7f9474f
learn.js.dev <learn.js.dev@gmail.com>
2021-11-03 13:41:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('./sw.js').then(response => {
+      console.log('==>', response);
+      // setTimeout(() => {
+      //   fetch(document.location).then(data => console.log('client fetch'))
+      // }, 4000)
+    })
 
-    setTimeout(() => {
-      fetch(document.location).then(data => console.log('client fetch'))
-    }, 4000)
+
   })
 }
