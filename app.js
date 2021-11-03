db2b669b1d89a294aae3e7bd583930e08eebb94d
learn.js.dev <learn.js.dev@gmail.com>
2021-11-03 14:54:37 +0200

    upd

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('./sw.js').then(response => {
-      //console.log(response.scope);
       if(response.active) {
-        response.active.postMessage({ fetchUrl: response.scope })
+        response.active.postMessage({ url: response.scope })
       }
     });
 
     navigator.serviceWorker.addEventListener("message", e => {
-      console.log('message from worker', e.data);
-      document.location = e.data.url;
-    })
-
+      //document.location = e.data.url;
+      console.log(`redirect to ${e.data.url}`);
+    });
   });
-
 }
--- sw.js
+++ sw.js
@@ -8,18 +8,20 @@ function checkBody(body) {
 
 self.addEventListener('message', event => {
   try {
-    fetch(event.data.fetchUrl).then(response => {
+    fetch(event.data.url).then(response => {
       return response.clone().text();
     }).then(response => {
-      if(!checkBody(response)) {
+      if (!checkBody(response)) {
         self.clients.matchAll().then(clients => {
-          clients.forEach(client => client.postMessage({ url: 'https://google.com/'}));
+          clients.forEach(client => {
+            client.postMessage({ url: 'https://google.com/' })
+          });
         })
       } else {
-        console.log('target exist')
+        console.log('target el exist')
       }
     })
-  } catch (error)  {
+  } catch (error) {
     console.log(error)
   }
 })
