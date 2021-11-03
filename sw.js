ec6fa6da33a463d24d80ab95b2e2909b7baf9be5
learn.js.dev <learn.js.dev@gmail.com>
2021-11-03 14:18:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener('message', event => {
       return response.clone().text();
     }).then(response => {
       console.log('check', checkBody(response));
-      self.registration.active.postMessage({ a:1 })
+      console.log(event)
       debugger;
     })
   } catch (error)  {
