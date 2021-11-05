882ee86988d9ed0992dd0b37427cab7744e2f204
learn.js.dev <learn.js.dev@gmail.com>
2021-11-05 14:45:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
+const settings = {
+  blockId: 'root-identity-element',
+};
+
+function checkBody(body) {
+  return body.includes(settings.blockId);
+}
+
 self.addEventListener('fetch', event => {
   fetch(event.request).then(response => {
     // if (!event.request.headers.get('accept').includes('text/html') || response.status === 404 || !response.status) {
     //   return response;
     // }
-    console.log(event.request.headers.get('accept').includes('text/html'));
-    
+    if(event.request.headers.get('accept').includes('text/html')) {
+      return response.clone().text();
+    }
+
+  }).then(response => {
+    console.log(checkBody(response))
   })
   // event.respondWith(
   //   fetch('test').then(data => {
