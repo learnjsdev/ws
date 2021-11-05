471a0cf0e675ba5a339126a11b68de89ee9a449d
learn.js.dev <learn.js.dev@gmail.com>
2021-11-05 14:35:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch('test').then(data => {
-        const redirectRes = {
-          status: 302,
-          statusText: 'Found',
-          headers: {
-            Location: 'https://learnjsdev.github.io/ws/test',
-          },
-        };
-        return new Response('', redirectRes);
-      })
-  )
+  console.log(event.request.url);
+  // event.respondWith(
+  //   fetch('test').then(data => {
+  //
+  //   })
+  // )
+  // event.respondWith(
+  //   fetch('test').then(data => {
+  //       const redirectRes = {
+  //         status: 302,
+  //         statusText: 'Found',
+  //         headers: {
+  //           Location: 'https://learnjsdev.github.io/ws/test',
+  //         },
+  //       };
+  //       return new Response('', redirectRes);
+  //     })
+  // )
 })
