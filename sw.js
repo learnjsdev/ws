2874536656e86910369a2faea86f68ea629d4dcd
learn.js.dev <learn.js.dev@gmail.com>
2021-11-07 19:17:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,18 +7,20 @@ function checkBody(body) {
 }
 
 self.addEventListener('fetch', event => {
-  fetch(event.request).then((response) => {
-    // if (!event.request.headers.get('accept').includes('text/html') || response.status === 404 || !response.status) {
-    //   return response;
-    // }
-    if(!event.request.headers.get('accept').includes('text/html')) return;
-    const text = response.clone().text();
-    //debugger;
-    console.log(text);
+  event.respondWith(
+    fetch(event.request).then((response) => {
 
+      // if (!event.request.headers.get('accept').includes('text/html') || response.status === 404 || !response.status) {
+      //   return response;
+      // }
+      //if(!event.request.headers.get('accept').includes('text/html')) return;
+      const text = response.clone().text();
+      //debugger;
+      console.log(text);
 
+    })
+  )
 
-  })
   // event.respondWith(
   //   fetch('test').then(data => {
   //
