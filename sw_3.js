bfe534d8c8793cbe973f3598420c04f9f70b2845
learn.js.dev <learn.js.dev@gmail.com>
2021-11-22 20:43:03 +0200

    Update sw_3.js

--- sw_3.js
+++ sw_3.js
@@ -8,9 +8,18 @@ function checkBody(body) {
 }
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request.destination === 'document', event.request)
+  if(event.request.destination === 'document') {
+    event.respondWith(
+      fetch(event.request).then(resp => {
+        return resp.clone().text();
+      }).then(data => {
+        console.log(data);
+        debugger;
+      })
+    )
+  }
 
-  debugger;
+  //debugger;
   // if (event.request.destination === 'document') {
   //   event.respondWith(
   //     fetch(event.request).then(resp => {
