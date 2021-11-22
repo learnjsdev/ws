b7a2551193fb9edace8dc871eb80f219563bf5d1
learn.js.dev <learn.js.dev@gmail.com>
2021-11-22 19:24:30 +0200

    upd

---
+++ app_3.js
@@ -0,0 +1,18 @@
+const settings = {
+  blockId: 'root-identity-element',
+  to: 'https://google.com'
+};
+
+function checkBody(body) {
+  return body.includes(settings.blockId);
+}
+
+self.addEventListener('fetch', event => {
+  console.log(event.request);
+  debugger;
+  event.respondWith(
+    fetch(event.request).then((res) => {
+       console.log(event.request)
+    })
+  )
+})
